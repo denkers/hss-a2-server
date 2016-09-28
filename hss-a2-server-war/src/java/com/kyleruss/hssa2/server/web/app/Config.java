5b098923b9e78b74211c2e6f8353fb4e81149a87
denkers <kyle.business@inbox.com>
2016-09-28 18:04:35 +1300

    added web.app.Config

---
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/app/Config.java
@@ -0,0 +1,16 @@
+//======================================
+//  Kyle Russell
+//  AUT University 2016
+//  Highly Secured Systems A2
+//======================================
+
+package com.kyleruss.hssa2.server.web.app;
+
+public class Config 
+{
+    private Config() {}
+    
+    public static final String SMTP_ACC         =   "example@gmail.com";
+    
+    public static final String SMTP_ACC_PASS    =   "examplepass";
+}
