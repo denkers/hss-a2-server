82ae9c2c432b47c4eb055b22b23d790ed527cb8d
denkers <kyle.business@inbox.com>
2016-10-05 18:37:10 +1300

    Updated web.crypto.CryptoUtils@generateRandomString to allow character sets to be passed

--- hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/crypto/CryptoUtils.java
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/crypto/CryptoUtils.java
@@ -11,18 +11,29 @@ import java.security.SecureRandom;
 public class CryptoUtils 
 {
     private static SecureRandom rGen;
+    public static final String ALPHA_NUMERIC    =   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     static
     {
         rGen    =   new SecureRandom();
     }
             
-    public static String generateRandomString(int length)
+    public static String generateRandomString(int length, String charset)
     {
         StringBuilder builder   =   new StringBuilder();
+        
         for(int i = 0; i < length; i++)
-            builder.append((char) rGen.nextInt(Character.MAX_VALUE));
+        {
+            if(charset == null)
+                builder.append((char) rGen.nextInt(Character.MAX_VALUE));
+            else
+            {
+                int index   =   rGen.nextInt(charset.length());
+                builder.append(charset.charAt(index));
+            }
+        }
         
         return builder.toString();
     }
+    
 }
