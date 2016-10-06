2cad6562a8ba2d1f0bf4a2cf34a7296b25078dd0
denkers <kyle.business@inbox.com>
2016-10-06 19:11:30 +1300

    Added initialization of server key pair in web.app.ServerKeyManager

--- hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/app/ServerKeyManager.java
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/app/ServerKeyManager.java
@@ -6,28 +6,33 @@
 
 package com.kyleruss.hssa2.server.web.app;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 public class ServerKeyManager 
 {
     private static ServerKeyManager instance;
+    private KeyPair serverKeyPair;
     
     private ServerKeyManager() {}
     
     protected void init()
     {
-        initPrivateKey();
-        initPublicKey();
-    }
-    
-    private void initPrivateKey()
-    {
+        try
+        {
+            KeyPairGenerator keyGen =   KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(512);
+            serverKeyPair   =   keyGen.generateKeyPair();
+        }
         
+        catch(NoSuchAlgorithmException e)
+        {
+            System.out.println("[Error] Failed to initialize server key pair: " + e.getMessage());
+        }
     }
     
-    private void initPublicKey()
-    {
-        
-    }
-    
+
     public static ServerKeyManager getInstance()
     {
         if(instance == null) instance = new ServerKeyManager();
