841af56d8b4138998e491126ecf7adaedb1a6473
denkers <kyle.business@inbox.com>
2016-10-06 18:37:35 +1300

    Added web.app.ContextListener

---
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/app/ContextListener.java
@@ -0,0 +1,21 @@
+//======================================
+//  Kyle Russell
+//  AUT University 2016
+//  Highly Secured Systems A2
+//======================================
+
+package com.kyleruss.hssa2.server.web.app;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextListener implements ServletContextListener
+{
+    @Override
+    public void contextInitialized(ServletContextEvent sce) 
+    {
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {}
+}
