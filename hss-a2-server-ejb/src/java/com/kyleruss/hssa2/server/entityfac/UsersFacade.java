611042d9f518cb1ccef48f04eba70d66bfc97420
denkers <kyle.business@inbox.com>
2016-10-07 17:10:46 +1300

    Added entity method server.entityfac.UsersFacade@createUserAccount for inserting new users

--- hss-a2-server-ejb/src/java/com/kyleruss/hssa2/server/entityfac/UsersFacade.java
+++ hss-a2-server-ejb/src/java/com/kyleruss/hssa2/server/entityfac/UsersFacade.java
@@ -7,6 +7,8 @@
 package com.kyleruss.hssa2.server.entityfac;
 
 import com.kyleruss.hssa2.server.entity.Users;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -27,5 +29,24 @@ public class UsersFacade extends AbstractFacade<Users>
     public UsersFacade() 
     {
         super(Users.class);
+    }
+    
+    public Entry<Boolean, String> createUserAccount(String phoneID, String name, String email)
+    {
+        boolean result  =   false;
+        String response;
+        
+        if(find(phoneID) != null)
+            response    =   "Account with this phone ID already exists";
+        
+        else
+        {
+            Users user  =   new Users(phoneID, name, email);
+            create(user);
+            result      =   em.contains(user);
+            response    =   result? "Successfully created account" : "Failed to create account";
+        }
+        
+        return new SimpleEntry<>(result, response);
     }
 }
