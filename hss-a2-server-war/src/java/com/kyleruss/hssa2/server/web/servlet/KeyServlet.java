7d1b1cfe802e557e799c0a07eccfd7b951466fa7
denkers <kyle.business@inbox.com>
2016-10-07 18:35:14 +1300

    Added web.servlet.KeyServlet

---
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/servlet/KeyServlet.java
@@ -0,0 +1,33 @@
+//======================================
+//  Kyle Russell
+//  AUT University 2016
+//  Highly Secured Systems A2
+//======================================
+
+package com.kyleruss.hssa2.server.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "KeyServlet", urlPatterns = {"/KeyServlet"})
+public class KeyServlet extends HttpServlet
+{
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException 
+    {
+        
+    }
+}
