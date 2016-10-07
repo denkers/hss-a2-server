e9762b9d10c713f1837bd3deb5e6e62d87edea1a
denkers <kyle.business@inbox.com>
2016-10-08 04:33:41 +1300

    Added ephemeralEncrypt and ephemeralDecrypt methods in CryptoController for encrypting/decrypting with PBE given a input password & salt

--- hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/app/CryptoController.java
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/app/CryptoController.java
@@ -7,27 +7,60 @@
 package com.kyleruss.hssa2.server.web.app;
 
 import com.kyleruss.hssa2.commons.Password;
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.KeySpec;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Encoder;
 
 public class CryptoController 
 {
     private static CryptoController instance;
     
-    public SecretKey generateEphemeralKey(Password password, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException
+    public String ephemeralDecrypt(Password password, String salt, String encodedCiphertext) 
+    throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchPaddingException, InvalidKeyException, 
+    InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
     {
-        char[] passChars            =   password.getPasswordCharacters();
-        byte[] saltBytes            =   salt.getBytes();
-        SecretKeyFactory keyFactory =   SecretKeyFactory.getInstance("PBEWithSHA256AndAES");
-        KeySpec keySpec             =   new PBEKeySpec(passChars, saltBytes, 256);
-        byte[] encodedKey           =   keyFactory.generateSecret(keySpec).getEncoded();
+        byte[] passwordBytes    =   password.getPassword().getBytes("UTF-8");
+        byte[] saltBytes        =   salt.getBytes("UTF-8");
+        SecretKeySpec secretKey =   new SecretKeySpec(passwordBytes, "AES");
+        IvParameterSpec iv      =   new IvParameterSpec(saltBytes);
         
-        return new SecretKeySpec(encodedKey, "AES");
+        Cipher cipherDec    =   Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipherDec.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        byte[] plaintext = cipherDec.doFinal(Base64.getDecoder().decode(encodedCiphertext.getBytes("UTF-8")));
+        return new String(plaintext);
+    }
+    
+    public String ephemeralEncrypt(Password password, String salt, String plaintext) 
+    throws NoSuchAlgorithmException, UnsupportedEncodingException, NoSuchPaddingException, InvalidKeyException, 
+    InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
+    {
+        byte[] passwordBytes    =   password.getPassword().getBytes("UTF-8");
+        byte[] saltBytes        =   salt.getBytes("UTF-8");
+        SecretKeySpec secretKey =   new SecretKeySpec(passwordBytes, "AES");
+        IvParameterSpec iv      =   new IvParameterSpec(saltBytes);
+        
+        Cipher cipher           =   Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+        byte[] ciphertext = cipher.doFinal(plaintext.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(ciphertext);
     }
     
     public static CryptoController getInstance()
--- hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/servlet/UserServlet.java
+++ hss-a2-server-war/src/java/com/kyleruss/hssa2/server/web/servlet/UserServlet.java
@@ -6,7 +6,21 @@
 
 package com.kyleruss.hssa2.server.web.servlet;
 
+import com.kyleruss.hssa2.commons.Password;
+import com.kyleruss.hssa2.server.web.app.CryptoController;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +30,29 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "UserServlet", urlPatterns = {"/UserServlet"})
 public class UserServlet extends HttpServlet 
 {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException 
+    {
+        String password =   "abcdefghrlemfyir";
+        String salt     =   "welymcturkcmpoei";
+        
+        try
+        {
+            String plaintext = "Hello World";
+            String ciphertext = CryptoController.getInstance().ephemeralEncrypt(new Password(password), salt, plaintext);
+            String dectext = CryptoController.getInstance().ephemeralDecrypt(new Password(password), salt, ciphertext);
+            
+            System.out.println("cipher text: " + ciphertext);
+            System.out.println("plain text: " + dectext);
+        }
+        
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -31,4 +68,6 @@ public class UserServlet extends HttpServlet
     {
         
     }
+    
+    
 }
