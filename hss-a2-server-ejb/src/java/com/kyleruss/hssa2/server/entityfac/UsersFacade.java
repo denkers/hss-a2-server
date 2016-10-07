0214a7677616d90528b0a59e54260b0b1064f898
denkers <kyle.business@inbox.com>
2016-10-07 18:04:32 +1300

    Added entity method server.entityfac.UsersFacade@getOnlineUsers for fetching user records in the passed list of user ids

--- hss-a2-server-ejb/src/java/com/kyleruss/hssa2/server/entityfac/UsersFacade.java
+++ hss-a2-server-ejb/src/java/com/kyleruss/hssa2/server/entityfac/UsersFacade.java
@@ -8,10 +8,16 @@ package com.kyleruss.hssa2.server.entityfac;
 
 import com.kyleruss.hssa2.server.entity.Users;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map.Entry;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Stateless
 public class UsersFacade extends AbstractFacade<Users>
@@ -48,5 +54,22 @@ public class UsersFacade extends AbstractFacade<Users>
         }
         
         return new SimpleEntry<>(result, response);
+    }
+    
+    public List<Users> getUserList()
+    {
+        return findAll();
+    }
+    
+    public List<Users> getOnlineUsers(List<String> userIDList)
+    {
+        CriteriaBuilder builder          =   em.getCriteriaBuilder();
+        CriteriaQuery<Users> query       =   builder.createQuery(entityClass);
+        Root<Users> from                 =   query.from(entityClass);
+        query.select(from);
+        query.where(from.get("id").in(userIDList));
+        
+        try { return em.createQuery(query).getResultList(); }
+        catch(NoResultException e) { return new ArrayList<>(); }
     }
 }
