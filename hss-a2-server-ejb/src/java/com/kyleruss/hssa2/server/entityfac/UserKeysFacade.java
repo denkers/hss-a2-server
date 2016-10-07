ebe4bdcce87b83bfcd297fd9844c49e295de844e
denkers <kyle.business@inbox.com>
2016-10-07 17:19:53 +1300

    Added entity method server.entityfac.UserKeysFacade@createuserKey for creating new public entries for a user

--- hss-a2-server-ejb/src/java/com/kyleruss/hssa2/server/entityfac/UserKeysFacade.java
+++ hss-a2-server-ejb/src/java/com/kyleruss/hssa2/server/entityfac/UserKeysFacade.java
@@ -7,6 +7,9 @@
 package com.kyleruss.hssa2.server.entityfac;
 
 import com.kyleruss.hssa2.server.entity.UserKeys;
+import com.kyleruss.hssa2.server.entity.Users;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -26,5 +29,27 @@ public class UserKeysFacade extends AbstractFacade<UserKeys>
     public UserKeysFacade() 
     {
         super(UserKeys.class);
+    }
+    
+    public Entry<Boolean, String> createUserKey(Users user, String publicKey)
+    {
+        boolean result  =   false;
+        String response;
+        
+        if(user == null) response   =   "User not found";
+        
+        else if(publicKey == null || publicKey.length() == 0) response  =   "Invalid key length";
+        
+        else
+        {
+            UserKeys keyRecord  =   new UserKeys();
+            keyRecord.setUserId(user);
+            keyRecord.setPubKey(publicKey);
+            create(keyRecord);
+            result      =   em.contains(keyRecord);
+            response    =   result? "Successfully create users public key" : "Failed to create user public key";   
+        }
+        
+        return new SimpleEntry<>(result, response);
     }
 }
